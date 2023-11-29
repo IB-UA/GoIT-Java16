4739cbe04bd9c6e2a9aed5ab40ab7f7b59bacbed
Ievgen Bardadym <ievgen.bardadym@xtendx>
2023-11-29 15:01:47 +0100

    refactor(streams_and_files): substitute custom implementation of hashmap with getOrDefault method, add more complex test for word counter
    Closes: homework-10

--- streams/src/main/java/org/example/WordCounter.java
+++ streams/src/main/java/org/example/WordCounter.java
@@ -5,16 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class WordCounter {
-    private static class DefaultHashMap<K,V> extends HashMap<K,V> {
-        protected V defaultValue;
-        public DefaultHashMap(V defaultValue) {
-            this.defaultValue = defaultValue;
-        }
-        @Override
-        public V get(Object k) {
-            return containsKey(k) ? super.get(k) : defaultValue;
-        }
-    }
 
     static void print(HashMap<String, Integer> map) {
         ArrayList<Integer> list = new ArrayList<>();
@@ -34,13 +24,13 @@ public class WordCounter {
     }
 
     public static void calc(File file) {
-        DefaultHashMap<String, Integer> counter = new DefaultHashMap<>(0);
+        HashMap<String, Integer> counter = new HashMap<>(0);
         try (Scanner scanner = new Scanner(file)){
             while (scanner.hasNextLine()) {
                String[] row = scanner.nextLine().split(" ");
                for (String word: row) {
                    if (!word.isBlank()) {
-                       counter.put(word, counter.get(word) + 1);
+                       counter.put(word, counter.getOrDefault(word, 0) + 1);
                    }
                }
             }
